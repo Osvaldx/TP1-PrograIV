4367588f5af78f0356c3b390eb42b84c03e85b53
osvaldx <romanonahuel24@gmail.com>
2025-09-04 14:21:29 -0300

    [+] added the github api

--- src/app/services/github-api.ts
+++ src/app/services/github-api.ts
@@ -1,9 +1,20 @@
 import { Injectable } from "@angular/core";
+import { GitHubUser } from "../interfaces/git-hub-user";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class GithubApi {
+
+  private apiURL: string = 'https://api.github.com/users/osvaldx';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getData(): Observable<GitHubUser> {
+    return this.httpClient.get<GitHubUser>(this.apiURL);
+  }
 
 }
