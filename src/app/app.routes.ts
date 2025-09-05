124a7d8e53e047ec4b230c98265415e57c47ed60
osvaldx <romanonahuel24@gmail.com>
2025-09-04 21:28:19 -0300

    [+] added the routes home,about,login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Login } from './pages/auth/login/login';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full"},
-    { path: "home", component: Home }
+    { path: "home", component: Home },
+    { path: "about", component: About },
+    { path: "login", component: Login }
 ];
