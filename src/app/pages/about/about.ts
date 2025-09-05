3b0f1f9dec412752d01dab6893136a4f7e43c974
osvaldx <romanonahuel24@gmail.com>
2025-09-04 21:14:35 -0300

    [+] added the github-api and getProfileData

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GithubApi } from '../../services/github-api';
+import { GitHubUser } from '../../interfaces/git-hub-user';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Header],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
+
+  private profile: GitHubUser | null = null;
+  private GitHubService = inject(GithubApi);
+
+  constructor() { }
+
+  public getProfileData() {
+    this.GitHubService.getData().subscribe((data) => { this.profile = (data) ? data : null });
+  }
 
 }
