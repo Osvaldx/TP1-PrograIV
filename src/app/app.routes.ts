7af130eead45e393bbf9958e38ca66abf2200589
osvaldx <romanonahuel24@gmail.com>
2025-09-06 22:36:20 -0300

    [+] added the route of register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Login } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full"},
     { path: "home", component: Home },
     { path: "about", component: About },
-    { path: "login", component: Login }
+    { path: "login", component: Login },
+    { path: "register", component: Register }
 ];
