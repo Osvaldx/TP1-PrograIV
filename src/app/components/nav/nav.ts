6cdf118380d09a35edeade33307a6d35ece144bc
osvaldx <romanonahuel24@gmail.com>
2025-09-06 22:35:05 -0300

    [+] added the logic of signOut

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Auth } from '../../services/database/auth';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +10,36 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Nav {
 
+  protected hideButton: boolean = false;
   protected hide: boolean = false;
 
-  constructor(private change: ChangeDetectorRef) { }
+  authService = inject(Auth);
+
+  constructor(private change: ChangeDetectorRef) {
+    this.authService.$user.subscribe((user) => {
+      if(user) {
+        this.hideButton = true;
+        this.change.markForCheck();
+      } else {
+        this.hideButton = false;
+        this.change.markForCheck();
+      }
+    })
+  }
 
   public hideMenu(): void {
     this.hide = !this.hide;
     this.change.detectChanges();
+  }
+
+  public async signOut() {
+    const { error } = await this.authService.signOut();
+
+    if(error) {
+      alert("ocurrio algo y no cerro");
+    } else {
+      this.authService.currentUser.next(null);
+    }
+
   }
 }
