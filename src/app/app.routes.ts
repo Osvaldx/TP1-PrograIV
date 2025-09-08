f59f97416220169fda744bca76382d2bfb1ea191
osvaldx <romanonahuel24@gmail.com>
2025-09-08 12:25:06 -0300

    [+] change the structured of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,26 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Login } from './pages/auth/login/login';
 import { Register } from './pages/auth/register/register';
+import { authGuard } from './guards/auth-guard';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { homeGuard } from './guards/home-guard';
 
 export const routes: Routes = [
-    { path: "", redirectTo: "home", pathMatch: "full"},
-    { path: "home", component: Home },
-    { path: "about", component: About },
-    { path: "login", component: Login },
-    { path: "register", component: Register }
+    {
+        path: "",
+        component: MainLayout,
+        canActivate: [homeGuard],
+        children: [
+            { path: "", component: Home },
+            { path: "about", component: About }
+        ]
+    },
+    {
+        path: "auth",
+        canActivate: [authGuard],
+        children: [
+            { path: "login", component: Login, },
+            { path: "register", component: Register }
+        ]
+    }
 ];
