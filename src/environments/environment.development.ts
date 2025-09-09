9345112995a9f6350084e6a986cb46b91dde16bc
osvaldx <romanonahuel24@gmail.com>
2025-09-08 21:41:22 -0300

    keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     supabase: {
-        API_URL: process.env['NG_APP_API_URL'] ?? '',
-        API_KEY: process.env['NG_APP_API_KEY'] ?? ''
+        API_URL: 'https://ahiygjpsqbvejnjpwane.supabase.co',
+        API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoaXlnanBzcWJ2ZWpuanB3YW5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4Njk1NDIsImV4cCI6MjA3MjQ0NTU0Mn0.HfqUE9jq-12z_tDnkS6FPxvYr5vt-N_MQRTPxdLu2Ys'
     }
 };
