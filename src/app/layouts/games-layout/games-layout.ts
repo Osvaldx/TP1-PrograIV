d5a4163171aba8569916eaea21f40d58d0da5879
osvaldx <romanonahuel24@gmail.com>
2025-09-20 16:53:45 -0300

    [+] game layout

---
+++ src/app/layouts/games-layout/games-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+
+@Component({
+  selector: 'app-games-layout',
+  imports: [Header],
+  templateUrl: './games-layout.html',
+  styleUrl: './games-layout.css'
+})
+export class GamesLayout {
+
+}
