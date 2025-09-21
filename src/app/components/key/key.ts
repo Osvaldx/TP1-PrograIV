308861eb43c72cc69f3158c1f34f8a14ab7563e4
osvaldx <romanonahuel24@gmail.com>
2025-09-21 03:42:31 -0300

    [+] the component key was created

---
+++ src/app/components/key/key.ts
@@ -0,0 +1,33 @@
+import { Component, effect, EventEmitter, Input, Output } from '@angular/core';
+import { LetterFormat } from '../../interfaces/letter-format';
+import { Ahorcado } from '../../pages/games/ahorcado/ahorcado';
+
+@Component({
+  selector: 'app-key',
+  imports: [],
+  templateUrl: './key.html',
+  styleUrl: './key.css'
+})
+
+export class Key {
+
+  @Input() letter?: string;
+  @Input() indexL?: number;
+  @Output() clickedLetter = new EventEmitter<LetterFormat>();
+  public disableKey: boolean = false;
+
+  constructor(private ahorcado: Ahorcado) {
+    effect(() => {
+      if(this.ahorcado.reset()) { this.disableKey = false };
+    })
+  }
+  
+  public sendLetter() {
+    this.clickedLetter.emit({letter: this.letter!, index: this.indexL!})
+    this.disableKey = true;
+  }
+
+  get isDisabled() {
+    return this.ahorcado.blockKeys() || this.disableKey;
+  }
+}
