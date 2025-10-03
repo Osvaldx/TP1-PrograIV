4f725bad32cfd488e045d8183ccf38a1149c21a9
Osvaldx <romanonahuel24@gmail.com>
2025-10-03 16:42:00 -0300

    [+] se implemento el GUARD de confirmExit en los juegos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { authGuard } from './guards/auth-guard';
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { GamesLayout } from './layouts/games-layout/games-layout';
 import { gamesGuard } from './guards/games-guard';
+import { confirmExitGuard } from './guards/confirm-exit-guard';
 
 export const routes: Routes = [
     {
@@ -26,10 +27,10 @@ export const routes: Routes = [
         component: GamesLayout,
         canMatch: [gamesGuard],
         children: [
-            { path: "ahorcado", loadComponent: () => import('./pages/games/ahorcado/ahorcado').then(m => m.Ahorcado) },
-            { path: "mayoromenor", loadComponent: () => import('./pages/games/mayoromenor/mayoromenor').then(m => m.Mayoromenor) },
-            { path: "preguntados", loadComponent: () => import('./pages/games/preguntados/preguntados').then(m => m.Preguntados) },
-            { path: "snake", loadComponent: () => import('./pages/games/snakegame/snakegame').then(m => m.Snakegame) }
+            { path: "ahorcado", canDeactivate: [confirmExitGuard], loadComponent: () => import('./pages/games/ahorcado/ahorcado').then(m => m.Ahorcado) },
+            { path: "mayoromenor", canDeactivate: [confirmExitGuard], loadComponent: () => import('./pages/games/mayoromenor/mayoromenor').then(m => m.Mayoromenor) },
+            { path: "preguntados", canDeactivate: [confirmExitGuard], loadComponent: () => import('./pages/games/preguntados/preguntados').then(m => m.Preguntados) },
+            { path: "snake", canDeactivate: [confirmExitGuard], loadComponent: () => import('./pages/games/snakegame/snakegame').then(m => m.Snakegame) }
         ]
     },
     
