1d955f1596c2762cd1303fef1b01395927adf86f
Osvaldx <romanonahuel24@gmail.com>
2025-10-07 16:12:00 -0300

    [+] se creo una directiva para cuando la serpiente se arrastra por el tablero

---
+++ src/app/directives/snake-cell.spec.ts
@@ -0,0 +1,8 @@
+import { SnakeCell } from './snake-cell';
+
+describe('SnakeCell', () => {
+  it('should create an instance', () => {
+    const directive = new SnakeCell();
+    expect(directive).toBeTruthy();
+  });
+});
