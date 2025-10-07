2986ecb72bfa6be6ad9aca0bebbf2ebc022158e1
Osvaldx <romanonahuel24@gmail.com>
2025-10-07 19:50:43 -0300

    [+] se termino el scoreboard del mayor o menor

--- src/app/components/scoreboards/mayoromenor-scoreboard/mayoromenor-scoreboard.ts
+++ src/app/components/scoreboards/mayoromenor-scoreboard/mayoromenor-scoreboard.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GamesType } from '../../../enums/games-type';
+import { TablesDB } from '../../../services/tables-db';
+import { TopColors } from '../../../directives/top-colors';
 
 @Component({
   selector: 'app-mayoromenor-scoreboard',
-  imports: [],
+  imports: [TopColors],
   templateUrl: './mayoromenor-scoreboard.html',
   styleUrl: './mayoromenor-scoreboard.css'
 })
 export class MayoromenorScoreboard {
 
+  public dataMayorOMenor = signal<any[] | null>(null);
+
+  constructor(private tableDB: TablesDB) { }
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.tableDB.getDataTableOfDB(GamesType.mayoromenor);
+    console.log(data);
+    this.dataMayorOMenor.set(data ?? null);
+  }
 }
