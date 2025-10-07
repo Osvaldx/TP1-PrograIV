5f84ae5b05b1e9916bd6ce6561bd7ebe0a36be80
Osvaldx <romanonahuel24@gmail.com>
2025-10-07 19:10:49 -0300

    [+] se mejoro los metodos de la tabla

--- src/app/services/tables-db.ts
+++ src/app/services/tables-db.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
 import { SupabaseService } from './database/supabase-service';
-
+import { GamesType } from '../enums/games-type';
+ 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TablesDB {
 
   constructor(private supabase: SupabaseService) {}
 
-  public async getDataOfTable() {
-    const { data, error } = await this.supabase.client.from('games').select("*");
-
-    if(error) {
-      console.log(error);
-      return [];
+  public async getDataTableOfDB(type: GamesType) {
+    if(type != "ahorcado") {
+      return this.getDataOfAnotherTable(type);
+    } else {
+      return this.getDataOfAhorcado();
     }
+  }
+
+  private async getDataOfAhorcado() {
+    const { data, error } = await this.supabase.client
+    .from('games')
+    .select("*")
+    .eq('game_type', 'ahorcado');
+
+    if(error) return console.log(error);
+
+    return data;
+  }
+  
+  private async getDataOfAnotherTable(type: GamesType) {
+    const { data, error } = await this.supabase.client
+    .from('games')
+    .select("*, profiles ( firstName )")
+    .eq('game_type', type)
+    .order('points', { ascending: false }).limit(5);
+
+    if(error) return console.log(error);
 
     return data;
   }
