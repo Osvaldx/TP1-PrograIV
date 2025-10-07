310e2890033bc29a8e574f334b0790c0740e4e1f
Osvaldx <romanonahuel24@gmail.com>
2025-10-07 16:11:38 -0300

    [+] se creo una directiva para cuando selecciona una categoria

---
+++ src/app/directives/selected-category.spec.ts
@@ -0,0 +1,8 @@
+import { SelectedCategory } from './selected-category';
+
+describe('SelectedCategory', () => {
+  it('should create an instance', () => {
+    const directive = new SelectedCategory();
+    expect(directive).toBeTruthy();
+  });
+});
