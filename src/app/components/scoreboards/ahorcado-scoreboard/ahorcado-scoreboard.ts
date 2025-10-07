668cd3fdb607c90127ef72e01a33760150a96cd2
Osvaldx <romanonahuel24@gmail.com>
2025-10-07 19:50:28 -0300

    [+] se termino el scoreboard del ahorcado

--- src/app/components/scoreboards/ahorcado-scoreboard/ahorcado-scoreboard.ts
+++ src/app/components/scoreboards/ahorcado-scoreboard/ahorcado-scoreboard.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GamesType } from '../../../enums/games-type';
+import { TablesDB } from '../../../services/tables-db';
+import { TopColors } from '../../../directives/top-colors';
 
 @Component({
   selector: 'app-ahorcado-scoreboard',
-  imports: [],
+  imports: [TopColors],
   templateUrl: './ahorcado-scoreboard.html',
   styleUrl: './ahorcado-scoreboard.css'
 })
 export class AhorcadoScoreboard {
 
+  public dataAhorcado = signal<any[] | null>(null);
+
+  constructor(private tableDB: TablesDB) { }
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.tableDB.getDataTableOfDB(GamesType.ahorcado);
+    console.log(data);
+    this.dataAhorcado.set(data ?? null);
+  }
 }
